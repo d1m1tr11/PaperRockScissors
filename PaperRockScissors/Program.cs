e62ed799b3f4092600bfb10b43431ee01ba86954
d1m1tr11 <dimitri.kaxniashvili@gmail.com>
2024-09-29 13:38:45 +0400

    Add project files.

---
+++ PaperRockScissors/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata;
+
+namespace PaperRockScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finished = false;
+            string player;
+            int computer;
+            string computerChoice;
+            Random rand = new Random();
+
+            while (!finished) 
+            {
+                computer = rand.Next(0,3);
+                var compChoice = (CompChoice.Choice)computer;
+                computerChoice = compChoice.ToString();
+
+
+
+                Console.WriteLine("enter \"paper\", \"rock\" or \"scissors\": ");
+                player = Console.ReadLine();
+                switch (player)
+                {
+                    case "paper":
+                        Console.WriteLine("paper " + computerChoice);
+                        break;
+
+                    case "rock":
+                        Console.WriteLine("rock " + computerChoice);
+                        break;
+
+                    case "scissors":
+                        Console.WriteLine("scissors " + computerChoice);
+                        break;
+
+                    default:
+                        Console.WriteLine("TRY AGAIN");
+                        break;
+                }
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+
+
+
+
+}
